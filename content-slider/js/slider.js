e02a10025d584eca76b44f8f082587ba289618e7
Jesse Garcia <techjess323@gmail.com>
2018-06-25 19:57:43 -0700

    Slider continued

--- content-slider/js/slider.js
+++ content-slider/js/slider.js
@@ -5,14 +5,40 @@ $(document).ready(function(){
   var autoswitch_speed = 4000; //auto slider speed;
 
   //add initial active class
-   $('.active').first().addClass('active');
+   $('.slide').first().addClass('active');
 
+
+   //Hide all divs
    $('.slide').hide();
 
 
-
+   //show first slide
    $('.active').show();
 
+//next button
+$('#next').on('click', function(){
+  $('.active').removeClass('active').addClass('oldActive');
+  if($('.oldActive').is(':last-child')){
+    $('.slide').first().addClass('active');
+  }else{
+    $('.oldActive').next().addClass('active');
+  }
+  $('.oldActive').removeClass('oldActive');
+  $('.slide').fadeOut(speed);
+  $('.active').fadeIn(speed);
+});
+
+$('#prev').on('click', function(){
+  $('.active').removeClass('active').addClass('oldActive');
+  if($('.oldActive').is(':first-child')){
+    $('.slide').last().addClass('active');
+  }else{
+    $('.oldActive').prev().addClass('active');
+  }
+  $('.oldActive').removeClass('oldActive');
+  $('.slide').fadeOut(speed);
+  $('.active').fadeIn(speed);
+});
 
 
 });
