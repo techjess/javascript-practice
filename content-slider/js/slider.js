08691b4f912c0dc1dea4f05786e30bccb24cd76a
Jesse Garcia <techjess323@gmail.com>
2018-06-25 19:38:45 -0700

    Javascript Practice Slider and Quiz

---
+++ content-slider/js/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  //Set some of variables
+  var speed = 500; //fade speed
+  var autoswitch = true; //fadespeed options
+  var autoswitch_speed = 4000; //auto slider speed;
+
+  //add initial active class
+   $('.active').first().addClass('active');
+
+   $('.slide').hide();
+
+
+
+   $('.active').show();
+
+
+
+});
